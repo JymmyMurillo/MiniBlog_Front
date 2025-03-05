9b92a647f9cde2c0d43e48a28afb075bf66a77af
Jymmy Murillo <murillojymmy@gmail.com>
2025-03-05 00:38:33 -0500

    Corrige tipos de dispatch para acciones asíncronas

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,17 +1,16 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import PostCard from "../components/posts/PostCard";
-import { loadPosts } from "../store/slices/postSlice";
+import { fetchPosts } from "../store/slices/postSlice";
+import { useAppDispatch } from "../hooks/useAppDispatch";
 
 const Posts = () => {
-  const dispatch = useDispatch();
-  const { items: posts, status } = useSelector(
-    (state: RootState) => state.posts
-  );
+  const dispatch = useAppDispatch();
+  const { posts, status } = useSelector((state: RootState) => state.posts);
 
   useEffect(() => {
-    dispatch(loadPosts({ page: 1, limit: 10 }));
+    dispatch(fetchPosts());
   }, [dispatch]);
 
   if (status === "loading") return <div>Cargando posts...</div>;
