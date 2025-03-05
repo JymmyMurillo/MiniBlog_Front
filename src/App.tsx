4ccb5591fe13a0785b214f299d9563aadd431176
Jymmy Murillo <murillojymmy@gmail.com>
2025-03-05 01:48:43 -0500

    Corrige estilos taildwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import MainLayout from "./layouts/MainLayout.tsx";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register";
 import Posts from "./pages/Posts.tsx";
-import PostDetail from "./pages/PostDetail.tsx";
+//import PostDetail from "./pages/PostDetail.tsx";
 import CreatePost from "./pages/CreatePost.tsx";
+import "./App.css";
+
 
 function App() {
   return (
@@ -17,7 +19,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Posts />} />
-            <Route path="/posts/:id" element={<PostDetail />} />
+          {/*  <Route path="/posts/:id" element={<PostDetail />} />*/}
             <Route path="/create-post" element={<CreatePost />} />
           </Route>
         </Route>
